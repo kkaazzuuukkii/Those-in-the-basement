d7fa373da00495b95e1650b75b9fae7196f81c31
Kazuki <116807921+kkaazzuuukkii@users.noreply.github.com>
2024-10-19 20:53:07 +0300

    fix

--- js/main1.js
+++ js/main1.js
@@ -2,70 +2,70 @@ const images = document.querySelector(".images");
 
 const general = [
   {
-    image: "/images/General/1.jpg",
+    image: "images/General/1.jpg",
   },
   {
-    image: "/images/General/2.jpg",
+    image: "images/General/2.jpg",
   },
   {
-    image: "/images/General/3.jpg",
+    image: "images/General/3.jpg",
   },
   {
-    image: "/images/General/4.jpg",
+    image: "images/General/4.jpg",
   },
   {
-    image: "/images/General/5.jpg",
+    image: "images/General/5.jpg",
   },
   {
-    image: "/images/General/6.jpg",
+    image: "images/General/6.jpg",
   },
   {
-    image: "/images/General/7.jpg",
+    image: "images/General/7.jpg",
   },
   {
-    image: "/images/General/8.jpg",
+    image: "images/General/8.jpg",
   },
   {
-    image: "/images/General/9.jpg",
+    image: "images/General/9.jpg",
   },
   {
-    image: "/images/General/10.jpg",
+    image: "images/General/10.jpg",
   },
   {
-    image: "/images/General/11.jpg",
+    image: "images/General/11.jpg",
   },
   {
-    image: "/images/General/12.jpg",
+    image: "images/General/12.jpg",
   },
   {
-    image: "/images/General/13.jpg",
+    image: "images/General/13.jpg",
   },
   {
-    image: "/images/General/14.jpg",
+    image: "images/General/14.jpg",
   },
   {
-    image: "/images/General/15.jpg",
+    image: "images/General/15.jpg",
   },
   {
-    image: "/images/General/16.jpg",
+    image: "images/General/16.jpg",
   },
   {
-    image: "/images/General/17.jpg",
+    image: "images/General/17.jpg",
   },
   {
-    image: "/images/General/18.jpg",
+    image: "images/General/18.jpg",
   },
   {
-    image: "/images/General/19.jpg",
+    image: "images/General/19.jpg",
   },
   {
-    image: "/images/General/20.jpg",
+    image: "images/General/20.jpg",
   },
   {
-    image: "/images/General/21.jpg",
+    image: "images/General/21.jpg",
   },
   {
-    image: "/images/General/22.jpg",
+    image: "images/General/22.jpg",
   },
 ];
 
--- js/main2.js
+++ js/main2.js
@@ -2,91 +2,91 @@ const images = document.querySelector(".images");
 
 const singles = [
   {
-    image: "/images/Singles/1.jpg",
+    image: "images/Singles/1.jpg",
   },
   {
-    image: "/images/Singles/2.jpg",
+    image: "images/Singles/2.jpg",
   },
   {
-    image: "/images/Singles/3.jpg",
+    image: "images/Singles/3.jpg",
   },
   {
-    image: "/images/Singles/4.jpg",
+    image: "images/Singles/4.jpg",
   },
   {
-    image: "/images/Singles/5.jpg",
+    image: "images/Singles/5.jpg",
   },
   {
-    image: "/images/Singles/6.jpg",
+    image: "images/Singles/6.jpg",
   },
   {
-    image: "/images/Singles/7.jpg",
+    image: "images/Singles/7.jpg",
   },
   {
-    image: "/images/Singles/8.jpg",
+    image: "images/Singles/8.jpg",
   },
   {
-    image: "/images/Singles/9.jpg",
+    image: "images/Singles/9.jpg",
   },
   {
-    image: "/images/Singles/10.jpg",
+    image: "images/Singles/10.jpg",
   },
   {
-    image: "/images/Singles/11.jpg",
+    image: "images/Singles/11.jpg",
   },
   {
-    image: "/images/Singles/12.jpg",
+    image: "images/Singles/12.jpg",
   },
   {
-    image: "/images/Singles/13.jpg",
+    image: "images/Singles/13.jpg",
   },
   {
-    image: "/images/Singles/14.jpg",
+    image: "images/Singles/14.jpg",
   },
   {
-    image: "/images/Singles/15.jpg",
+    image: "images/Singles/15.jpg",
   },
   {
-    image: "/images/Singles/16.jpg",
+    image: "images/Singles/16.jpg",
   },
   {
-    image: "/images/Singles/17.jpg",
+    image: "images/Singles/17.jpg",
   },
   {
-    image: "/images/Singles/18.jpg",
+    image: "images/Singles/18.jpg",
   },
   {
-    image: "/images/Singles/19.jpg",
+    image: "images/Singles/19.jpg",
   },
   {
-    image: "/images/Singles/20.jpg",
+    image: "images/Singles/20.jpg",
   },
   {
-    image: "/images/Singles/21.jpg",
+    image: "images/Singles/21.jpg",
   },
   {
-    image: "/images/Singles/22.jpg",
+    image: "images/Singles/22.jpg",
   },
   {
-    image: "/images/Singles/23.jpg",
+    image: "images/Singles/23.jpg",
   },
   {
-    image: "/images/Singles/24.jpg",
+    image: "images/Singles/24.jpg",
   },
   {
-    image: "/images/Singles/25.jpg",
+    image: "images/Singles/25.jpg",
   },
   {
-    image: "/images/Singles/26.jpg",
+    image: "images/Singles/26.jpg",
   },
   {
-    image: "/images/Singles/27.jpg",
+    image: "images/Singles/27.jpg",
   },
   {
-    image: "/images/Singles/28.jpg",
+    image: "images/Singles/28.jpg",
   },
   {
-    image: "/images/Singles/29.jpg",
+    image: "images/Singles/29.jpg",
   },
 ];
 
